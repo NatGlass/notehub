1f773984e9738ed408b4f353b9d05b36659de691
Visibyte <nathanglass26@gmail.com>
2023-10-12 15:05:40 +0100

    dark mode

--- app/(landing)/_components/logo.tsx
+++ app/(landing)/_components/logo.tsx
@@ -3,7 +3,20 @@ import Image from "next/image";
 const Logo = () => {
   return (
     <div className="hidden md:flex items-center gap-x-2">
-      <Image src="/logo.png" width={100} height={100} alt="Logo" />
+      <Image
+        src="/logo.png"
+        width={100}
+        height={100}
+        alt="Logo"
+        className="dark:hidden"
+      />
+      <Image
+        src="/logo-light.png"
+        width={100}
+        height={100}
+        alt="Logo"
+        className="light:hidden"
+      />
     </div>
   );
 };
