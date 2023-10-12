f78ea28a4bbca7e661867c0cc58e9e6e8ee7db3e
Visibyte <nathanglass26@gmail.com>
2023-10-12 10:36:32 +0100

    custom colours

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 const Home = () => {
   return (
     <div>
-      <Button>Hello!</Button>
+      <Button>Primary</Button>
+      <Button variant="ghost">Ghost</Button>
+      <Button variant="outline">Outline</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="link">Link</Button>
+      <Input />
+
       <h1 className="text-2xl">Is this DM Sans?</h1>
     </div>
   );
