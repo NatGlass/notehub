7b5cff196a550ca098178082dbc3f7629869f2ad
Visibyte <nathanglass26@gmail.com>
2023-10-15 11:09:24 +0100

    documents page

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,5 +1,36 @@
+"use client";
+import Image from "next/image";
+import { useUser } from "@clerk/clerk-react";
+import { PlusCircle } from "lucide-react";
+import { Button } from "@/components/ui/button";
+
 const DocumentsPage = () => {
-  return <div>DocumentsPage</div>;
+  const { user } = useUser();
+  return (
+    <div className="h-full flex flex-col items-center justify-center space-y-4">
+      <Image
+        src="/empty.png"
+        width={300}
+        height={300}
+        alt="Empty"
+        className="dark:hidden"
+      />
+      <Image
+        src="/empty-dark.png"
+        width={300}
+        height={300}
+        alt="Empty"
+        className="hidden dark:block"
+      />
+      <h2 className="text-lg font-medium">
+        Hi {user?.firstName}, welcome to your Note Hub.
+      </h2>
+      <Button>
+        <PlusCircle className="w-4 h-4 mr-2" />
+        Create a note
+      </Button>
+    </div>
+  );
 };
 
 export default DocumentsPage;
